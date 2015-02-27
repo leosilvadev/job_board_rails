56c8cc452a337450163723c979e9bb20b9987905
Guilherme Pontes <guilhermepontes@msn.com>
2015-02-27 11:15:10 -0300

    :nail_care:

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,26 +3,24 @@ class CompaniesController < ApplicationController
   def index
     @companies = Company.all
   end
-  
-  def new 
+
+  def new
     @company = Company.new
   end
-  
+
   def create
     @company = Company.new(company_params)
-    
+
     if @company.save
       redirect_to root_path, notice: 'Company saved successfully!'
-    
     else
       render action: 'new'
-      
     end
   end
-  
+
   private
   def company_params
-      params.require(:company).permit(:name, :email, :password, :password_confirmation)
+    params.require(:company).permit(:name, :email, :password, :password_confirmation)
   end
-  
+
 end
